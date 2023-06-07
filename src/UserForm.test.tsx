6775e9921754e0c773943664ba212907bafbe64b
juanmiguel431 <juanmiguel431@gmail.com>
2023-06-06 22:45:04 -0400

    UserForm.test.tsx added

---
+++ src/UserForm.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import UserForm from './UserForm';
+
+test('it shows two inputs and a button', () => {
+  // render the component
+  render(<UserForm
+    onUserAdd={() => {
+    }}/>)
+
+  // Manipulate the component or find an element in it.
+  const inputs = screen.getAllByRole('textbox');
+  const button = screen.getByRole('button');
+
+  // Assertion - make sure the component is doing
+  // what we expect it to do.
+  expect(inputs).toHaveLength(2);
+  expect(button).toBeInTheDocument();
+});
