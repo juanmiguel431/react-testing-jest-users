a9bbe216af163122e42c9e7b852631e9dc6964bd
juanmiguel431 <juanmiguel431@gmail.com>
2023-06-08 17:46:00 -0400

    React testing library updated.

--- src/UserForm.test.tsx
+++ src/UserForm.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import user from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import UserForm from './UserForm';
 
 test('it shows two inputs and a button', () => {
