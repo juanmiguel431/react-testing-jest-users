b8891c8d1f3dcc9d981fe27fc5a813e805f24428
juanmiguel431 <juanmiguel431@gmail.com>
2023-06-08 21:38:54 -0400

    UserList.test.tsx - render email and name of each user test implemented.

--- src/__tests__/UserList.test.tsx
+++ src/__tests__/UserList.test.tsx
@@ -22,5 +22,22 @@ test('render one row per user', async () => {
 });
 
 test('render email and name of each user', async () => {
+  const users: User[] = [
+    { name: 'jame', email: 'jane@jane.com'},
+    { name: 'sam', email: 'sam@sam.com'},
+  ];
+  render(<UserList users={users} />);
 
+  for (let user of users) {
+    const name = screen.getByRole('cell', {
+      name: user.name
+    });
+
+    const email = screen.getByRole('cell', {
+      name: user.email
+    });
+
+    expect(name).toBeInTheDocument()
+    expect(email).toBeInTheDocument()
+  }
 });
