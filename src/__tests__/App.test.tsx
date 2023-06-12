83bf841bc04cba82fb47ba88884f7688e605a1e5
juanmiguel431 <juanmiguel431@gmail.com>
2023-06-12 19:59:00 -0400

    App.test - can receive a new user and show it on a list . test added.

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('can receive a new user and show it on a list', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const nameInput = screen.getByRole('textbox', {
+    name: /name/i
+  });
+
+  const emailInput = screen.getByRole('textbox', {
+    name: /email/i
+  });
+
+  const button = screen.getByRole('button');
+
+  await userEvent.click(nameInput);
+  await userEvent.keyboard('jane');
+  await userEvent.click(emailInput);
+  await userEvent.keyboard('jane@jane.com');
+
+  await userEvent.click(button);
+
+  // screen.debug();
+  const name = screen.getByRole('cell', { name: 'jane' });
+  const email = screen.getByRole('cell', { name: 'jane@jane.com' });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
 });
